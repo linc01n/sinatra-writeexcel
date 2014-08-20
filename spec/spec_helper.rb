574d374c226b9720726c252707aaa7e54435c417
Lincoln Lee <git@lincoln.hk>
2014-08-20 23:38:25 +0800

    Move codeclimate code to the top

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,8 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 ENV['RACK_ENV'] = 'test'
 
 require_relative './mock/app'
 require 'rspec'
 require 'rack/test'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
